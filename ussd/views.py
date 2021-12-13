89f0f6cea254d8eec21772d20781f99a4e4596c8
DENYSE <denysedufitimana@gmail.com>
2021-12-13 11:42:40 +0200

    my project

--- ussd/views.py
+++ ussd/views.py
@@ -9,8 +9,8 @@ def  welcome(request):
     return render(request, 'index.html')
 
 #  python3 -m pip install africastalking
-AfricasUsername='tuganimana01@gmail.com'
-api_key ='1526a36fc4c257d18d07bcfd53b0d18324ce969a5cd6981a35abfa6028b259ac'
+AfricasUsername='denysedufitimana@gmail.com'
+api_key ='e206468a9df4c9be697e337f35a1a0caab9f549e2135af9f417d990beb246dbc'
 africastalking.initialize(AfricasUsername,api_key)
 
 @csrf_exempt
@@ -27,24 +27,24 @@ def ussdApp(request):
         response =""
         #  main menu for our application
         if text == '':
-            response =  "CON Murakaza neza kuri Idafarm \n"
-            response += "1. Kwandikisha igihingwa \n"
-            response += "2. Kumenya ingengabihe \n"
+            response =  "CON Welcome to E-Butchery \n"
+            response += "1. Kugura \n"
+            response += "2. Kumenya menu\n"
         elif text == '1':
 
-            response = "CON Hitamo igihingwa \n"
-            response += "1. Ibinyomoro \n"
-            response += "2. Indimu"
+            response = "CON Hitamo ubwoko bw'inyama\n"
+            response += "1. inka  \n"
+            response += "2. ihene"
         elif text == '1*1':
-            product="Ibinyomoro"
-            response = "CON shyiramo ubuso bw'ubutaka bwawe bw' "+str(product)+"\n"
+            product="inka"
+            response = "CON shyiramo ibiro ukeneye' "+str(product)+"\n"
         elif category =='1*1' and int(len(level)) == 3 and str(level[2]) in  str(level):
-            response = "CON Uwo mubufatanyije \n"
+            response = "CON Aderesi \n"
         elif category =='1*1' and int(len(level)) == 4 and str(level[3]) in  str(level):
-            response = "CON Shyiramo nimero y'irangamuntu yuwo mufatanyije \n"
+            response = "CON injiza amafaranga wishyure\n"
         elif category =='1*1' and int(len(level)) == 5 and str(level[4]) in  str(level):
             # save the data into the database
-            category='Ibinyomoro'
+            category='Inka'
             sizeOfland=level[2]
             names= level[3]
             idnumber = level[4]
@@ -58,18 +58,18 @@ def ussdApp(request):
             idnumber=idnumber,
             )
             insert.save()
-            response = "END Murakoze kwiyandikisha kuri Ida farm \n"
+            response = "END Murakoze guhahana natwe\n"
 
 
         elif text == '1*2':
-            product ="Indimu"
-            response ="CON shyiramo ubuso bw'ubutaka bwawe bw' "+str(product)+"\n"
+            product ="Inkoko"
+            response ="CON ibiro ukeneye' "+str(product)+"\n"
         elif category =='1*2' and int(len(level)) == 3 and str(level[2]) in  str(level):
-            response = "CON Uwo mubufatanyije \n"
+            response = "CON  \n"
         elif category =='1*2' and int(len(level)) == 4 and str(level[3]) in  str(level):
-            response = "CON Shyiramo nimero y'irangamuntu yuwo mufatanyije \n"
+            response = "CON aderesi\n"
         elif category =='1*2' and int(len(level)) == 5 and str(level[4]) in  str(level):
-            category='Indimu'
+            category='Inkoko'
             sizeOfland=level[2]
             names= level[3]
             idnumber = level[4]
@@ -83,39 +83,39 @@ def ussdApp(request):
             idnumber=idnumber,
             )
             insert.save()
-            response = "END Murakoze kwiyandikisha kuri Ida farm \n"
+            response = "END Murakoze guhahana natwe \n"
          
         #  ======================== INGENGABIHE==================
         elif text == '2':
-            response = "CON Hitamo igihe \n "
-            response += "1. Rimwe mukwezi \n"
-            response += "2. Kabiri Mukwezi \n"
-            response += "3. Buri gihe"
+            response = "CON Hitamo isaha ikunogeye \n "
+            response += "1. 80:00-11:00 \n"
+            response += "2. 1:00-5:00 \n"
+            response += "3. Anytime"
         elif text == '2*1':
             # save the data
-            insertData(
-                category='Rimwe',
-                sessionID=session_id,
-                phoneNumber=phone_number
-            )
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe rimwe mukwezi"
-        elif text == '2*2':
-            insertData(
-                category='Kabiri',
-                sessionID=session_id,
-                phoneNumber=phone_number
-            )
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe kabiri mukwezi"
-        elif text == '2*3':
-            insertData(
-                category='Burigihe',
-                sessionID=session_id,
-                phoneNumber=phone_number
-            )
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe Buri munsi"
+        #     insertData(
+        #         category='Rimwe',
+        #         sessionID=session_id,
+        #         phoneNumber=phone_number
+        #     )
+        #     response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe rimwe mukwezi"
+        # elif text == '2*2':
+        #     insertData(
+        #         category='Kabiri',
+        #         sessionID=session_id,
+        #         phoneNumber=phone_number
+        #     )
+        #     response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe kabiri mukwezi"
+        # elif text == '2*3':
+        #     insertData(
+        #         category='Burigihe',
+        #         sessionID=session_id,
+        #         phoneNumber=phone_number
+        #     )
+        #     response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe Buri munsi"
 
-        else:
-            response = "END Ukanze ibitaribyo, ongera mukanya"
-        return HttpResponse(response)
-    else:
-        return HttpResponse('we are on ussd app')
+        # else:
+        #     response = "END Ukanze ibitaribyo, ongera mukanya"
+        # return HttpResponse(response)
+    # else:
+         return HttpResponse('we are on ussd app')
